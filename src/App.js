051dcc2470e4fa2e317c80ee328111d96c0557c8
Clinton Manakane <manakanejustin@gmail.com>
2021-05-07 13:36:59 +0200

    before you search anything the address is empty and doesnt have any data so i use conditional statement to make it work

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { useState } from "react";
 import Search from "./Search";
 
 function App() {
-  const [address, setAddress] = useState({});
-
-  const addressParts = address.results[0].address_components;
+  const [address, setAddress] = useState();
 
   console.log(address);
 
@@ -13,16 +11,17 @@ function App() {
     <main>
       <Search setAddress={setAddress} />
 
-      <div>
-        {/* {addressParts.map((item, index) => (
-          <div key={index}>
-            <p>Long name:: {item.long_name}</p>
-            <p>Short name:: {item.short_name}</p>
-            <p>Type of address component:: {item.types}</p>
-            <hr />
-          </div>
-        ))} */}
-      </div>
+      {address
+        ? address.results[0].address_components.map((item, index) => (
+            <div key={index}>
+              <p>Long name:: {item.long_name}</p>
+              <p>Short name:: {item.short_name}</p>
+              <p>Type of address component:: {item.types}</p>
+              <hr />
+            </div>
+          ))
+        : ""}
+      <div></div>
     </main>
   );
 }
